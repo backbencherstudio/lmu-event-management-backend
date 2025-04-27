fe634cc95587f3cae7a71d1f615b7cd2816da696
Tariq-Monowar <hassainseam@gmail.com>
2025-04-27 10:21:13 +0600

    fixt

--- src/main.ts
+++ src/main.ts
@@ -9,14 +9,20 @@ async function bootstrap() {
       origin: [
         'http://192.168.4.31:3000',
         'http://192.168.4.31:*',
+        'http://192.168.4.31:3001',
+        'http://192.168.4.31:3002',
+        'http://192.168.4.31:3003',
+        'http://192.168.4.31:3004',
         'http://localhost:3000',
+        'http://localhost:3001',
+        'http://localhost:3002',
         'https://lmu-event-management-frontend.vercel.app',
         'https://lmu-event-management-frontend-akuy6y82z-bbsfullstacks-projects.vercel.app'
       ],
       credentials: true,
     },
   });
-  const port = process.env.PORT || 3001;
+  const port = process.env.PORT || 3002;
   app.use(morgan('dev'));
 
   app.useGlobalPipes(new ValidationPipe({
