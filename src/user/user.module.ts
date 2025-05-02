b926f83228fd06fd986a206dd1ccf7695bde2c2d
Tariq-Monowar <hassainseam@gmail.com>
2025-05-02 10:51:07 +0600

    update JwtAuthGuard

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -5,10 +5,16 @@ import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
-    JwtModule.register({
-      global: true,
-      secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '24h' },
+    // JwtModule.register({
+    //   global: true,
+    //   secret: process.env.JWT_SECRET,
+    //   signOptions: { expiresIn: '24h' },
+    // }),
+    JwtModule.registerAsync({
+      useFactory: async () => ({
+        secret: process.env.JWT_SECRET,
+        signOptions: { expiresIn: '365d' },
+      }),
     }),
   ],
   controllers: [UserController],
